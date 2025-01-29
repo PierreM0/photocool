2fa9677fcd36bb5939c9e2f4d78b235a05bba106
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-01-29 11:21:27 +0100

    Ajout case grille

---
+++ Views/CaseGrille.axaml.cs
@@ -0,0 +1,13 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace photocool.Views;
+
+public partial class CaseGrille : UserControl
+{
+    public CaseGrille()
+    {
+        InitializeComponent();
+    }
+}
