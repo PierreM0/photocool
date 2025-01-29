f3c64f7c563e08354095affd0d22718c0ba8dc74
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-01-29 11:56:35 +0100

    Test grille dynamique

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia.Controls;
 
 namespace photocool.Views;
@@ -7,5 +8,30 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        const int NB_IMAGES = 25;
+        const int NB_COLUMNS = 4;
+        const int NB_ROWS = 7;
+
+        for (int i = 0; i < NB_ROWS; i++)
+        {
+            Grille.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Auto) });
+        }
+        
+        for (int i = 0; i < NB_COLUMNS; i++)
+        {
+            Grille.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Auto) });
+        }
+
+        for (int i = 0; i < NB_IMAGES; i++)
+        {
+            int row = i / NB_COLUMNS;
+            int col = i % NB_COLUMNS;
+            Console.WriteLine(row + ", " + col);
+            CaseGrille caseGrille = new CaseGrille();
+            Grid.SetRow(caseGrille, row);
+            Grid.SetColumn(caseGrille, col);
+            Grille.Children.Add(caseGrille);
+        }
     }
 }
