cff34f6b9c26de8ecb650fab8f2df67ea7a10617
Laumon1u <adam.laumon9@etu.univ-lorraine.fr>
2025-04-07 16:26:55 +0200

    maj docker compose et ajout structure sql

---
+++ photocool.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : mariadb:3306
+-- Généré le : lun. 07 avr. 2025 à 14:21
+-- Version du serveur : 10.6.21-MariaDB-ubu2004
+-- Version de PHP : 8.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `photocool`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Images`
+--
+
+CREATE TABLE `Images` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(10) NOT NULL,
+  `image` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Tag`
+--
+
+CREATE TABLE `Tag` (
+  `id` int(11) NOT NULL,
+  `tag` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `TagImages`
+--
+
+CREATE TABLE `TagImages` (
+  `image_id` int(11) NOT NULL,
+  `tag_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tag_famille`
+--
+
+CREATE TABLE `tag_famille` (
+  `tag_fils` int(11) NOT NULL,
+  `tag_parent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `Images`
+--
+ALTER TABLE `Images`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nom` (`nom`);
+
+--
+-- Index pour la table `Tag`
+--
+ALTER TABLE `Tag`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `tag` (`tag`);
+
+--
+-- Index pour la table `TagImages`
+--
+ALTER TABLE `TagImages`
+  ADD PRIMARY KEY (`image_id`,`tag_id`),
+  ADD KEY `tag_id` (`tag_id`);
+
+--
+-- Index pour la table `tag_famille`
+--
+ALTER TABLE `tag_famille`
+  ADD PRIMARY KEY (`tag_fils`,`tag_parent`),
+  ADD KEY `tag_parent` (`tag_parent`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Images`
+--
+ALTER TABLE `Images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT pour la table `Tag`
+--
+ALTER TABLE `Tag`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `TagImages`
+--
+ALTER TABLE `TagImages`
+  ADD CONSTRAINT `TagImages_ibfk_1` FOREIGN KEY (`image_id`) REFERENCES `Images` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `TagImages_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `Tag` (`id`) ON DELETE CASCADE;
+
+--
+-- Contraintes pour la table `tag_famille`
+--
+ALTER TABLE `tag_famille`
+  ADD CONSTRAINT `tag_famille_ibfk_1` FOREIGN KEY (`tag_fils`) REFERENCES `Tag` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `tag_famille_ibfk_2` FOREIGN KEY (`tag_parent`) REFERENCES `Tag` (`id`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
