b98a5c80f11ef3bbc1a5532b5e3301776b07cc7d
Laumon1u <adam.laumon9@etu.univ-lorraine.fr>
2025-04-07 16:18:47 +0200

    fonctions images get,delete, ajouter tag, enlever tag

--- DB/DatabaseManager.cs
+++ DB/DatabaseManager.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using Avalonia.Win32.Interop.Automation;
 using MySql.Data.MySqlClient;
 using Tmds.DBus.Protocol;
@@ -12,17 +16,23 @@ public class DatabaseManager
 /// Fonction qui renvoie tout les tags dans un reader 
 /// </summary>
 /// <returns>MySqlDataReader</returns>
-    public static MySqlDataReader getAllTags()
+    public static List<string> getAllTags()
     {
+        List<string> tags = new List<string>();
         using (MySqlConnection connection = new MySqlConnection(_connectionString))
         {
             connection.Open();
             string query = "SELECT * FROM `Tag`";
             MySqlCommand command = new MySqlCommand(query, connection);
-            return command.ExecuteReader();
-
-            
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    tags.Add(reader.GetString("tag"));
+                }
+            }
         }
+        return tags;
     }
 /// <summary>
 /// Fonction qui ajoute un tag et renvoi son id.
@@ -179,5 +189,247 @@ public class DatabaseManager
                 }
             }
         }
+    }
+/// <summary>
+/// renvoie les enfants directs d'un tag
+/// </summary>
+/// <param name="tag">tag donné</param>
+/// <param name="childs"> dictionnaire des enfants, peut être null</param>
+/// <returns>le dictionnaire des enfants du tags (paire ID,Tag)</returns>
+    public static Dictionary<long,string> getChildsOfTag(string tag,Dictionary<long,string> childs=null)
+    {
+        if (childs is null)
+        {
+            
+            childs = new Dictionary<long, string>();
+        }
+
+        long id = getTagId(tag);
+        if (id != -1L)
+            using (MySqlConnection connection = new MySqlConnection(_connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM `tag_famille` WHERE `tag_parent`=@tag";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@tag", id);
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            long idOfChild = reader.GetInt32("tag_fils");
+                            string tagOfChild = getTag(idOfChild);
+                            
+                            childs.Add(idOfChild,tagOfChild);
+                        }
+                    }
+                }
+            }
+        return childs;
+    }
+/// <summary>
+/// renvoie le tag à partir d'un id bdd,renvoi un string vide s'il n'existe pas
+/// </summary>
+/// <param name="id">id du tag</param>
+/// <returns>tag du tag</returns>
+    public static string getTag(long id)
+    {
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string query = "SELECT * FROM `Tag` WHERE `id`=@id";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@id", id);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        return reader.GetString("tag");
+                    }
+                }
+                
+            }
+        }
+        return "";
+    }
+/// <summary>
+/// ajoute une image en bdd
+/// </summary>
+/// <param name="imagePath">chemin du fichier de l'image</param>
+/// <param name="tag">tag à ajouter à l'image</param>
+/// <param name="nom">nom de l'image</param>
+/// <returns>l'id de l'image insérée</returns>
+    public static long addImage(string imagePath, string tag, string nom)
+    {
+        byte[] imageData = File.ReadAllBytes(imagePath);
+        long id = -1L;
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO Images (nom, image) values (@nom, @image);";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@nom", nom);
+                command.Parameters.AddWithValue("@image", imageData);
+                command.ExecuteNonQuery();
+                id = command.LastInsertedId;
+            }
+            
+        }
+        addTagToImage(nom, tag, id);
+        return id;
+    }
+/// <summary>
+/// ajoute un tag à une image
+/// </summary>
+/// <param name="nom">nom de l'image</param>
+/// <param name="tag">tag a ajouter</param>
+/// <param name="imageId">id de l'image (optionnel mais accelère)</param>
+/// <param name="tagId">id du tag (optionnel mais accelère)</param>
+/// <returns> l'id de la relation créée</returns>
+    public static long addTagToImage(string nom, string tag,long imageId =-1L, long tagId=-1L)
+    {
+        long tagImageId = -1L;
+        if (tagId == -1L)
+        {
+            
+            tagId = getTagId(tag);
+            Console.WriteLine(tagId);
+        }
+            
+        if (imageId == -1L)
+            imageId = getImageId(nom);
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string query = "INSERT INTO `TagImages` (`image_id`, `tag_id`) VALUES (@imageId, @tagId);";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@imageId", imageId);
+                
+                command.Parameters.AddWithValue("@tagId", tagId);
+                command.ExecuteNonQuery();
+                tagImageId = command.LastInsertedId;
+            }
+        }
+        return tagImageId;
+    }
+    /// <summary>
+    /// Donne l'id d'une image avec son nom
+    /// </summary>
+    /// <param name="nom">nom de l'image</param>
+    /// <returns>l'id de l'image et -1 si elle n'est pas trouvée</returns>
+    public static long getImageId(string nom)
+    {
+        long imageId = -1L;
+        string query = "SELECT * FROM `Images` WHERE `nom`=@nom";
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@nom", nom);
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        imageId = reader.GetInt32("id");
+                    }
+                }
+            }
+        }
+        return imageId;
+    }
+/// <summary>
+/// enlève une image de la bdd
+/// </summary>
+/// <param name="nom">nom de l'image</param>
+/// <param name="imageId">id de l'image (optionnel)</param>
+    public static void removeImage(string nom, long imageId = -1L)
+    {
+        if (imageId == -1L)
+        {
+            imageId = getImageId(nom);
+        }
+
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string query = "DELETE FROM `Images` WHERE `Images`.`Id`=@imageId";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@imageId",imageId);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+/// <summary>
+/// Enleve un tag d'une image
+/// </summary>
+/// <param name="nom">nom de l'image</param>
+/// <param name="tag">tag de l'image</param>
+/// <param name="imageId">id de l'immage (optionnel)</param>
+/// <param name="tagId">id du tag (optionnel)</param>
+    public static void removeTagFromImage(string nom, string tag, long imageId = -1L, long tagId = -1L)
+    {
+        if(imageId == -1L)
+            imageId = getImageId(nom);
+        if(tagId == -1L)
+            tagId = getTagId(tag);
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string query = "delete from `TagImages` WHERE `image_id`=@imageId AND `tag_id`=@tagId";
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                command.Parameters.AddWithValue("@imageId", imageId);
+                command.Parameters.AddWithValue("@tagId", tagId);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+    /// <summary>
+    /// Récupère les images avec les tags spécifiés
+    /// </summary>
+    /// <param name="tagIds">liste d'id des tags, si aucun tag n'est fourni, récupère toute les images</param>
+    /// <returns>un dictionnaire contenant nom et data de l'image.</returns>
+    public static Dictionary<string, byte[]> getImages(List<long> tagIds = null)
+    {
+        Dictionary<string, byte[]> images = new Dictionary<string, byte[]>();
+        using (MySqlConnection connection = new MySqlConnection(_connectionString))
+        {
+            connection.Open();
+            string query;
+            if(tagIds is null)
+                query = "SELECT * FROM  Images";
+            else
+            {
+                var ids = tagIds.Select((id,l) =>$"@id{l}" ).ToList();
+                string inClause = string.Join(",", ids);
+                query = $"SELECT DISTINCT i.id, i.nom, i.image FROM `Images` i INNER JOIN `TagImages` ti ON i.id = ti.image_id INNER JOIN Tag t ON t.id = ti.tag_id WHERE t.id IN ({inClause});";
+                
+            }
+
+            using (MySqlCommand command = new MySqlCommand(query, connection))
+            {
+                if(tagIds is not null)
+                    for (int i = 0; i < tagIds.Count; i++)
+                    {
+                        command.Parameters.AddWithValue($"@id{i}", tagIds[i]);
+                    }
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        byte[] imageData = (byte[])reader["image"];
+                        images.Add(reader.GetString("nom"), imageData);
+                    }
+                }
+            }
+        }
+
+        return images;
     }
 }
--- Views/NavBar.axaml.cs
+++ Views/NavBar.axaml.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Collections.Generic;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+using MySql.Data.MySqlClient;
 using photocool.DB;
 
 namespace photocool.Views;
@@ -17,15 +20,34 @@ public partial class NavBar : UserControl
  */
     private void TagButton_Handler(object? sender, RoutedEventArgs e)
     {
-        DatabaseManager.addTagWithParent("testAddTagWithParent","test");
+        try
+        {
+            //DatabaseManager.addImage("C:\\Users\\Adam\\Pictures\\images_coolmdr\\RDT_20240424_0206313735683715627756656.jpg",
+            //    "test", "usopp");
+            List<long> tagIds = new List<long>();
+            tagIds.Add(11L);
+            tagIds.Add(1L);
+            foreach (var VARIABLE in DatabaseManager.getImages(tagIds))
+            {
+                Console.WriteLine(VARIABLE.Key);
+            }
+            
+        }
+        catch (MySqlException ex)
+        {
+            if (ex.Number == 1062) // 1062 = Duplicate entry
+            {
+                Console.WriteLine("Doublon détecté !");
+            }
+            else
+            {
+                Console.WriteLine("Erreur MySQL : " + ex.Message);
+            }
+        }
+
     }
-    /**
-     * TODO: ajouter image bdd
-     * ajouter une image avec une image, un nom et au moins 1 tag
-     * modifier les tags d'une image
-     * récupérer les images
-     * récupérer images avec n tags (OU et ET)
-     * supprimer image
+    /**TODO:
+     * récupérer images avec n tags (ET) OU is done
      */
     private void ImageButton_Handler(object? sender, RoutedEventArgs e)
     {
