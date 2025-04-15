c3b62d413efd32d0f701015fe9391360cbcf65bc
Qasim Boubekeur <qasim.boubekeur@proton.me>
2025-04-15 18:18:33 +0200

    fenetre nouveau tag

---
+++ Views/NewTagWindow.axaml.cs
@@ -0,0 +1,24 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+
+namespace photocool.Views;
+
+public partial class NewTagWindow : Window
+{
+    public NewTagWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Cancel_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    private void OK_Click(object? sender, RoutedEventArgs e)
+    {
+        throw new System.NotImplementedException();
+    }
+}
